91e027024d0d720872e86bf686f5c7b4bb89cb83
ELBA22 <137318270+ELBA22@users.noreply.github.com>
2023-12-04 17:17:47 -0500

    feat: :sparkles: Empezando

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Multiplicacion Rusa");
+        Console.WriteLine("1. Ingrese un numero multiplicador: ");
+        int multiplicador = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("2. Ingrese otro numero multiplicando: ");
+        int multiplicando = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("3. SALIR");
+        Console.WriteLine("Ingrese una opcion: ");
+        int opcion = Convert.ToInt32(Console.ReadLine());
+        
+
+    }
+}
